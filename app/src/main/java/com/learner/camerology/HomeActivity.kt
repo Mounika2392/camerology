e9ca7f08ccb4d95b5b5a3d7b30cb8a46e2013a0b
Mounika <mounika.r36@outlook.com>
2021-11-16 11:58:59 -0500

    Added navigation components

--- app/src/main/java/com/learner/camerology/HomeActivity.kt
+++ app/src/main/java/com/learner/camerology/HomeActivity.kt
@@ -2,12 +2,31 @@ package com.learner.camerology
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar()?.hide(); // hide the title bar
         setContentView(R.layout.activity_home)
+        setUpNavigation()
     }
 
+    private fun setUpNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+       val graph = navController.navInflater.inflate(R.navigation.nav_graph)
+        navController.graph = graph
+
+        val toolBar = findViewById<MaterialToolbar>(R.id.cm_tool_bar)
+        toolBar.setupWithNavController(navController, AppBarConfiguration(graph))
+    }
 }
